665759d75e40c1eef53c80e9dff37dfcb46dbc8f
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-15 19:42:01 +0300

    Add navbar component with HTML structure, CSS styles, and toggle functionality

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update((val) => !val);
+  }
+}
