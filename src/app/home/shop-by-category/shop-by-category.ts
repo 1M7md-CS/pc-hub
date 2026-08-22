9d9d69009b9e93043320e8c11b6c699d23b7b30a
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-22 16:13:38 +0300

    Add ShopByCategory component with HTML template and CSS styles

---
+++ src/app/home/shop-by-category/shop-by-category.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SectionHeader } from '../../section-header/section-header';
+
+@Component({
+  selector: 'app-shop-by-category',
+  imports: [SectionHeader],
+  templateUrl: './shop-by-category.html',
+  styleUrl: './shop-by-category.css',
+})
+export class ShopByCategory {}
