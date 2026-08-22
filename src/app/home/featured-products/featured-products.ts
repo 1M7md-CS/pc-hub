078d35703ad050fa4163b5d4c8badae9f17a29af
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-22 23:45:11 +0300

    feat: add FeaturedProducts component with HTML and CSS

---
+++ src/app/home/featured-products/featured-products.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SectionHeader } from '../../shared/ui/section-header/section-header';
+
+@Component({
+  selector: 'app-featured-products',
+  imports: [SectionHeader],
+  templateUrl: './featured-products.html',
+  styleUrl: './featured-products.css',
+})
+export class FeaturedProducts {}
