88fc8cd175e814a093d701f888c1a3ad3b39a077
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-22 16:11:22 +0300

    Add ShopByCategory component to home layout and update imports

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
-import { Navbar } from "./navbar/navbar";
+import { Navbar } from './navbar/navbar';
+import { ShopByCategory } from "./shop-by-category/shop-by-category";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Navbar],
+  imports: [Hero, Navbar, ShopByCategory],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
