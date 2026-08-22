98329fb52caf8ec1d6955985f71478af010e6127
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-22 23:44:32 +0300

    fix: add FeaturedProducts component to home imports

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { Navbar } from './navbar/navbar';
 import { ShopByCategory } from './shop-by-category/shop-by-category';
+import { FeaturedProducts } from "./featured-products/featured-products";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Navbar, ShopByCategory],
+  imports: [Hero, Navbar, ShopByCategory, FeaturedProducts],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
