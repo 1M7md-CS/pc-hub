9d2345e63fde4771c138e5c668b40a86dae063e5
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-25 15:32:18 +0300

    feat: add promo banner component with styles and HTML structure

---
+++ src/app/home/promo-banner/promo-banner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-banner',
+  imports: [],
+  templateUrl: './promo-banner.html',
+  styleUrl: './promo-banner.css',
+})
+export class PromoBanner {}
