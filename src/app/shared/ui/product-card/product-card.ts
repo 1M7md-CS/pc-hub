2274f7ded268abd7558cb399ac652afc29dfd267
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-25 04:15:42 +0300

    feat: add ProductCard component with HTML and CSS for product display

---
+++ src/app/shared/ui/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component, computed, input } from '@angular/core';
+import { Product } from '../../../models/product-model';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  product = input.required<Product>();
+  iconPaths = computed(() => this.product().iconPaths ?? []);
+}
