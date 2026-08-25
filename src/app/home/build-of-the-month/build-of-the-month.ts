aed24080e9c7435e351449b4cdb8064cadc03ccf
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-25 18:35:42 +0300

    feat: add Build of the Month component with styles and HTML structure

---
+++ src/app/home/build-of-the-month/build-of-the-month.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-build-of-the-month',
+  templateUrl: './build-of-the-month.html',
+  styleUrl: './build-of-the-month.css',
+})
+export class BuildOfTheMonth {}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -6,10 +6,11 @@ import { Navbar } from './navbar/navbar';
 import { ShopByCategory } from './shop-by-category/shop-by-category';
 import { WhyChooseUs } from "./why-choose-us/why-choose-us";
 import { PromoBanner } from "./promo-banner/promo-banner";
+import { BuildOfTheMonth } from "./build-of-the-month/build-of-the-month";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Navbar, ShopByCategory, FeaturedProducts, WhyChooseUs, PromoBanner],
+  imports: [Hero, Navbar, ShopByCategory, FeaturedProducts, WhyChooseUs, PromoBanner, BuildOfTheMonth],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
