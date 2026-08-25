399f32a3bf5bfb78e8889874b712cad80479b813
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-25 19:18:16 +0300

    feat: add productOfTheMonth method and update featuredProducts to return multiple items

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -7,6 +7,12 @@ import { Category, Product } from '../models/product-model';
 export class ProductsService {
   private httpClient = inject(HttpClient);
 
+  get productOfTheMonth() {
+    return this.httpClient
+      .get<Product[]>('data/pre-built-pcs.json')
+      .pipe(map((product) => product[0]));
+  }
+
   get categories() {
     return this.httpClient.get<Category[]>('data/categories.json');
   }
@@ -20,11 +26,11 @@ export class ProductsService {
       storage: this.httpClient.get<Product[]>('data/storage.json'),
     }).pipe(
       map(({ pre_built_pc, gpu, cpu, ram, storage }) => [
-        pre_built_pc[0],
-        gpu[0],
-        cpu[0],
-        ram[0],
-        storage[0],
+        ...pre_built_pc.slice(0, 2),
+        ...gpu.slice(0, 2),
+        ...cpu.slice(0, 2),
+        ...ram.slice(0, 2),
+        ...storage.slice(0, 2),
       ]),
     );
   }
