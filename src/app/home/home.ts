713241c18b049990e77ea95b5292418572fc798e
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-25 15:32:26 +0300

    feat: add WhyChooseUs and PromoBanner components to home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,10 +4,12 @@ import { FeaturedProducts } from './featured-products/featured-products';
 import { Hero } from './hero/hero';
 import { Navbar } from './navbar/navbar';
 import { ShopByCategory } from './shop-by-category/shop-by-category';
+import { WhyChooseUs } from "./why-choose-us/why-choose-us";
+import { PromoBanner } from "./promo-banner/promo-banner";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Navbar, ShopByCategory, FeaturedProducts],
+  imports: [Hero, Navbar, ShopByCategory, FeaturedProducts, WhyChooseUs, PromoBanner],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
