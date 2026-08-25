fd973f35ae8e161b325a036aa13766289a0f4048
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-25 18:35:53 +0300

    refactor: remove unused banner-cta styles and update promo-banner link class

--- src/app/home/promo-banner/promo-banner.ts
+++ src/app/home/promo-banner/promo-banner.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-promo-banner',
-  imports: [],
   templateUrl: './promo-banner.html',
   styleUrl: './promo-banner.css',
 })
