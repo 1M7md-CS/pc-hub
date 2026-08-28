0e1ca79311aa5cac5e9b5bea387448e911007bef
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-28 17:18:05 +0300

    feat: update call-to-action button with router link for improved navigation

--- src/app/home/build-of-the-month/build-of-the-month.ts
+++ src/app/home/build-of-the-month/build-of-the-month.ts
@@ -2,11 +2,13 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ProductsService } from '../../services/products';
 import { Product } from './../../models/product-model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-build-of-the-month',
   templateUrl: './build-of-the-month.html',
   styleUrl: './build-of-the-month.css',
+  imports: [RouterLink],
 })
 export class BuildOfTheMonth implements OnInit {
   private productsService = inject(ProductsService);
