39837e5a160e688c95e426226d2aeed60ddbc625
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-28 17:21:10 +0300

    feat: add routerLink to promo banner for improved navigation

--- src/app/home/promo-banner/promo-banner.ts
+++ src/app/home/promo-banner/promo-banner.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-promo-banner',
   templateUrl: './promo-banner.html',
   styleUrl: './promo-banner.css',
+  imports: [RouterLink],
 })
 export class PromoBanner {}
