b01c711eeabece255dc85b78c9c67679c43841e2
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-28 17:21:23 +0300

    feat: implement CategoryProducts component with routing and product retrieval

---
+++ src/app/category-products/category-products.ts
@@ -0,0 +1,47 @@
+import { Component, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
+import { Product } from '../models/product-model';
+import { ProductsService } from '../services/products';
+import { ProductCard } from '../shared/ui/product-card/product-card';
+import { SectionHeader } from "../shared/ui/section-header/section-header";
+
+@Component({
+  selector: 'app-category-products',
+  imports: [RouterLink, ProductCard, SectionHeader],
+  templateUrl: './category-products.html',
+  styleUrl: './category-products.css',
+})
+export class CategoryProducts implements OnInit {
+  slug = input.required<string>();
+
+  private productsService = inject(ProductsService);
+  private destroyRef = inject(DestroyRef);
+
+  categoryTitle = signal<string | undefined>(undefined);
+  products = signal<Product[] | undefined>(undefined);
+  isEmptyProducts = signal(false);
+
+  ngOnInit(): void {
+    this.productsService
+      .getCategory(this.slug())
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (category) => this.categoryTitle.set(category?.title),
+      });
+
+    this.productsService
+      .getProducts(this.slug())
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (products) => {
+          if (products.length === 0) {
+            this.isEmptyProducts.set(true);
+            return;
+          }
+
+          this.products.set(products);
+        },
+      });
+  }
+}
