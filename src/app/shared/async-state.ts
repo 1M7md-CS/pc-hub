11f815a8de2953d8e4814fe78458a2bc7c3a0a98
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-29 18:01:24 +0300

    feat: add LoadState class and loadState function for managing async loading states

---
+++ src/app/shared/async-state.ts
@@ -0,0 +1,24 @@
+import { DestroyRef, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
+
+export class LoadState<T> {
+  readonly isLoading = signal<boolean>(true);
+  readonly isEmpty = signal<boolean>(false);
+  readonly data = signal<T | undefined>(undefined);
+  readonly error = signal<string | null>(null);
+}
+
+export function loadState<T>(source: Observable<T>, state: LoadState<T>, destroyRef: DestroyRef) {
+  source.pipe(takeUntilDestroyed(destroyRef)).subscribe({
+    next: (data) => {
+      state.isLoading.set(false);
+      if (Array.isArray(data) && data.length === 0) state.isEmpty.set(true);
+      state.data.set(data);
+    },
+    error: (error) => {
+      state.isLoading.set(false);
+      state.error.set(error);
+    },
+  });
+}
