1a8f290a71ac7dd2df91e1e365d206ae06f046e3
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-29 18:34:33 +0300

    feat: add state card error component with styles and template

---
+++ src/app/shared/ui/state-card-error/state-card-error.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-state-card-error',
+  imports: [],
+  templateUrl: './state-card-error.html',
+  styleUrl: './state-card-error.css',
+})
+export class StateCardError {}
