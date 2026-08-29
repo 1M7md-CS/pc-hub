dcf32dc64c5f3b68d0a4481a3ff0489f45a6fcee
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-29 14:47:49 +0300

    feat: add ProductSkeleton component with HTML and CSS for loading state

---
+++ src/app/shared/ui/product-skeleton/product-skeleton.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Skeleton } from "../skeleton/skeleton";
+
+@Component({
+  selector: 'app-product-skeleton',
+  imports: [Skeleton],
+  templateUrl: './product-skeleton.html',
+  styleUrl: './product-skeleton.css',
+})
+export class ProductSkeleton {
+
+}
