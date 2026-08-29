bbdbd3722f00db877d24c7c20f7aafa723117090
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-29 18:04:22 +0300

    refactor: reorganize imports in home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 
 import { FeaturedProducts } from './featured-products/featured-products';
+import { Footer } from './footer/footer';
 import { Hero } from './hero/hero';
 import { Navbar } from './navbar/navbar';
+import { ProductOfTheMonth } from './product-of-the-month/product-of-the-month';
+import { PromoBanner } from './promo-banner/promo-banner';
 import { ShopByCategory } from './shop-by-category/shop-by-category';
-import { WhyChooseUs } from "./why-choose-us/why-choose-us";
-import { PromoBanner } from "./promo-banner/promo-banner";
-import { BuildOfTheMonth } from "./build-of-the-month/build-of-the-month";
-import { Testimonials } from "./testimonials/testimonials";
-import { Footer } from "./footer/footer";
+import { Testimonials } from './testimonials/testimonials';
+import { WhyChooseUs } from './why-choose-us/why-choose-us';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Navbar, ShopByCategory, FeaturedProducts, WhyChooseUs, PromoBanner, BuildOfTheMonth, Testimonials, Footer],
+  imports: [
+    Hero,
+    Navbar,
+    ShopByCategory,
+    FeaturedProducts,
+    WhyChooseUs,
+    PromoBanner,
+    ProductOfTheMonth,
+    Testimonials,
+    Footer,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
