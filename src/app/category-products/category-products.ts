022eb375eb5f77c05c87b81278c656247b89fd24
1M7md-CS <mohammadhammad464@gmail.com>
2026-08-29 18:35:05 +0300

    feat: replace error message div with state card error component in category products

--- src/app/category-products/category-products.ts
+++ src/app/category-products/category-products.ts
@@ -6,10 +6,11 @@ import { LoadState, loadState } from '../shared/async-state';
 import { ProductCard } from '../shared/ui/product-card/product-card';
 import { ProductSkeleton } from '../shared/ui/product-skeleton/product-skeleton';
 import { SectionHeader } from '../shared/ui/section-header/section-header';
+import { StateCardError } from "../shared/ui/state-card-error/state-card-error";
 
 @Component({
   selector: 'app-category-products',
-  imports: [RouterLink, ProductCard, SectionHeader, ProductSkeleton],
+  imports: [RouterLink, ProductCard, SectionHeader, ProductSkeleton, StateCardError],
   templateUrl: './category-products.html',
   styleUrl: './category-products.css',
 })
